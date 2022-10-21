36ffc38e40d4e595f5e4c0053140a2e2a87e4458
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-10-21 20:08:29 +0700

    Remigrate model again

--- general_user/models.py
+++ general_user/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=64, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name']
 class RekeningBank(models.Model):
     MANDIRI = "MANDIRI"
     BNI = "BNI"
@@ -23,11 +28,6 @@ class RekeningBank(models.Model):
     nama_bank = models.CharField(max_length=8, choices=BANK_CHOICES, default=MANDIRI)
     no_rekening = models.CharField(max_length=16, unique=True)
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(max_length=64, unique=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name']
-
 
 class GeneralUser(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
@@ -35,8 +35,3 @@ class GeneralUser(models.Model):
     akun_bank = models.OneToOneField(RekeningBank, on_delete=models.RESTRICT)
     no_ponsel = models.CharField(max_length=16, unique=True)
 
-# @receiver(post_save, sender=CustomUser)
-# def update_profile_signal(sender, instance, created, **kwargs):
-#     if created:
-#         GeneralUser.objects.create(user=instance)
-#     instance.person.save()
