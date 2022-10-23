037bd7e9d5ec5bfe34b60cde77f05a67d697a03d
Hana <hana@Hanas-MacBook-Pro.local>
2022-10-23 22:46:46 +0700

    test push models

--- customer_service/models.py
+++ customer_service/models.py
@@ -1,3 +1,12 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Pertanyaan(models.Model):
+    teks_pertanyaan = models.TextField();
+
+    
+class FAQ(models.Model):
+    pertanyaan = models.OneToOneField(Pertanyaan, on_delete=CASCADE)
+    jawaban = models.TextField()
+
