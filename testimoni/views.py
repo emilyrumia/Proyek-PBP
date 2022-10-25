dcd814481e28467e3a43b627be78331c4e833c39
DaHerdi <danendra.herdian1243@gmail.com>
2022-10-25 21:33:26 +0700

    adding models and form testimoni

--- testimoni/views.py
+++ testimoni/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request) :
+
+# Method untuk menampilkan testimoni dari semua pengguna
+def show_testimoni(request) :
     return HttpResponse("Ini testimoni pengguna")
+
+# Method untuk menambahkan testimoni (dibutuhkan login akun)
+@login_required(login_url='/login')
+def add_testimoni(request) :
+    return  
