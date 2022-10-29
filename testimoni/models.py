e680af0d65626ba9b5754e3fe9af070d0b02d4c3
DaHerdi <danendra.herdian1243@gmail.com>
2022-10-29 13:37:30 +0700

    model change

--- testimoni/models.py
+++ testimoni/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from general_user.models import GeneralUser
 from django.contrib.auth.models import User
 
 # Create your models here.
 class TestimoniList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
     nama = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     pesan = models.TextField()
