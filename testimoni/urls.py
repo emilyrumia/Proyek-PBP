be93f7434c6fc6f45a44d5070baf0a6735225972
DaHerdi <danendra.herdian1243@gmail.com>
2022-10-29 22:01:54 +0700

    adding carousel to testimoni

--- testimoni/urls.py
+++ testimoni/urls.py
@@ -6,5 +6,6 @@ app_name = "testimoni"
 urlpatterns = [
     path('', views.show_testimoni, name="show_testimoni"),
     path('json/', views.show_testimoni_json, name="show_testimoni_json"),
+    path('json-random/', views.show_random_testimoni, name="show_random_testimoni"),
     path('add-testimoni/', views.add_testimoni, name="add_testimoni")
 ]
--- testimoni/views.py
+++ testimoni/views.py
@@ -1,4 +1,4 @@
-import datetime
+import random
 from general_user.models import GeneralUser
 from testimoni.models import TestimoniList
 
@@ -14,8 +14,17 @@ def show_testimoni(request) :
     # Menampilkan semua testimoni (maupun bagi yang belum login)
     list_testimoni = TestimoniList.objects.all()
 
+    initiate_list_testimoni = list(TestimoniList.objects.all()) 
+    random_testimoni = random.sample(initiate_list_testimoni, 3)
+    first_random_testimoni = random_testimoni[0]
+    second_random_testimoni = random_testimoni[1]
+    third_random_testimoni = random_testimoni[2]
+
     context = {
-        'list_testimoni' : list_testimoni
+        'list_testimoni' : list_testimoni,
+        'first_random_testimoni' : first_random_testimoni,
+        'second_random_testimoni' : second_random_testimoni,
+        'third_random_testimoni' : third_random_testimoni
     }
 
     return render(request, "testimoni.html", context)
@@ -25,6 +34,12 @@ def show_testimoni_json(request):
     list_testimoni = TestimoniList.objects.all()
     return HttpResponse(serializers.serialize('json', list_testimoni), content_type="application/json")
 
+
+def show_random_testimoni(request):
+    list_testimoni = list(TestimoniList.objects.all())
+    random_testimoni = random.sample(list_testimoni, 3)
+    
+    return HttpResponse(serializers.serialize('json', random_testimoni), content_type="application/json")
 
 # Method untuk menambahkan testimoni (dibutuhkan login akun)
 @login_required(login_url='/login')
