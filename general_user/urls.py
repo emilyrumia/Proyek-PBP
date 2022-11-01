79c3faafce7b51cf8eba268a820d22667d8b5b57
RahBry <bryan.naufal1@gmail.com>
2022-11-02 06:47:26 +0700

    add html, not finished

--- general_user/urls.py
+++ general_user/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', views.login_user, name="login"),
     path('register/', views.register, name="register"),
     path('logout/', views.logout_user, name="logout"),
-    path('galang/json/', views.show_json_galang, name="show_json_galang"),
-    path('lelang/json/', views.show_json_lelang, name="show_json_lelang"),
+    # path('galang/json/', views.show_json_galang, name="show_json_galang"),
+    # path('lelang/json/', views.show_json_lelang, name="show_json_lelang"),
 
 ]
