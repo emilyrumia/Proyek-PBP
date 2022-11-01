c9c950f9d30e850c16729e9837de3415bed7ce9f
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-11-01 12:16:01 +0700

    temp

--- resipien/urls.py
+++ resipien/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from resipien import views
-from resipien.views import show_buat_galang, show_daftar_galang, show_detail_galang, show_json, show_add_komentar, show_json_komentar
+from resipien.views import delete_galang, show_buat_galang, show_daftar_galang, show_detail_galang, show_json
 
 
 app_name = "resipien"
@@ -11,6 +11,5 @@ urlpatterns = [
     path('daftar/', show_daftar_galang, name='daftar'),
     path('detail/<int:id>/', show_detail_galang, name='detail'),
     path('json/', show_json, name='json'),
-    path('add/<int:id>/', show_add_komentar, name='add-komentar'),
-    path('json_komentar/<int:id>/', show_json_komentar, name="json-komentar")
+    path('delete-galang/<int:id>/', delete_galang, name='deleteGalang'),
 ]
